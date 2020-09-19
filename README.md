99b2ffcfdca4ab5d104295f407320ea0479c4535
Elitmiar <elitmiar@gmail.com>
2020-09-19 19:27:47 +0200

    Added all files to repo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# url_query_builder
+# URL Query Builder
 Super simple TypeScript URL Query Builder
+
+## Install
+``npm install url_query_builder --save``
+
+## Usage
+
+```
+import BuildHTTPQuery from 'url_query_builder';
+
+const qb = new BuildHTTPQuery();
+
+// Setting the url
+qb.url = 'https://your-url.com'
+
+qb.addQueryParam('page', 1);
+qb.addQueryParam('limit', 10);
+qb.addQueryParam('inactive', true);
+qb.addQueryParam('name', 'joe');
+
+// Outputs: https://your-url.com?page=1&limit=10&inactive=true&name=joe
+console.log(qb.getQueryString()); 
+```
+
---
+++ index.ts
@@ -0,0 +1,28 @@
+export default class BuildHTTPQuery {
+	private _url: string = '';
+
+	get url(): string {
+		return this._url;
+	}
+
+	set url(value: string) {
+		this._url = value;
+	}
+
+	private params: { key: string; value: string | number | boolean }[] = [];
+
+	public addQueryParam(key: string, value: string | number | boolean ) {
+		this.params.push({ key, value });
+	}
+
+	public getQueryString(): string {
+		let qs: string = this.url + '?';
+		for (const p of this.params) {
+			if (p.key.length > 0) {
+				qs += `${p.key}=${p.value}&`;
+			}
+		}
+
+		return qs.slice(0, -1);
+	}
+}
